f7eff207c05206e42d15cd756d2b614f5c2b6ed5
JuaMV <juan.menares.vega@ciisa.cl>
2022-10-26 18:38:06 -0300

    first commit

---
+++ server/libs/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs-extra";
+
+cloudinary.config({
+  cloud_name: "dgrh1ybqq",
+  api_key: "284946887318364",
+  api_secret: "BtJbrwnHqvqVHZXhEZ6Ejdhq78Q",
+});
+
+export const uploadImage = async (filePath) => {
+  const result = await cloudinary.uploader.upload(filePath, {
+    folder: "posts",
+  });
+  const image = {
+    url: result.secure_url,
+    public_id: result.public_id,
+  }
+  await fs.remove(filePath);
+  return image
+};
+
+/**asi estaba antes
+ export const uploadImage = async (filePath) => {
+  return await cloudinary.uploader.upload(filePath, {
+    folder: "posts",
+  });
+};
+ */
+export const deleteImage = async (id) => {
+  return await cloudinary.uploader.destroy(id);
+};
